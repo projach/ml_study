49d310cbc5696d753be9fe793e2ce04fb6192622
projach <iakovosplane123@gmail.com>
2022-12-16 03:35:21 +0200

    fixed the return to be a list

--- frontend/app2.py
+++ frontend/app2.py
@@ -1,17 +1,26 @@
 import streamlit as st
 import requests
+import json
+
+
 
 def fetch_somth():
 	base_url = "http://127.0.0.1:8000/results"
 	resp = requests.post(base_url)
-	return resp.json()
+	return json.dumps(resp.json())
+
+def formating_string():
+    return  fetch_somth().replace('[','').split(',')
+    
+    
+
 
 def main():
 	st.title("Main App")
 
 	if st.button("Retrieve"):
-		results = fetch_somth()
-		st.json(results)
+		results = formating_string()
+		st.write(f"{results[0]}")
 
 
 
